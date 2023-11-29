1d74f579b81defafdf313b0ffbd7baa6dd6e7cfb
Leunam77 <juan.ma6531@gmail.com>
2023-11-29 18:15:39 -0400

    cambio a MVC

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'services/firebase_options.dart';
+import 'models/services/firebase_options.dart';
 
 void main() async{
   await Firebase.initializeApp(
